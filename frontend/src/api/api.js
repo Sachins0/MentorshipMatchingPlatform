7b81040d760f16f09c9912124655da401e32a3f4
Sachins0 <sachins.ssingh0@gmail.com>
2024-12-19 14:00:36 +0530

    added backend link

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/v1', // Backend URL
+  baseURL: 'https://mentorshipmatchingplatformbackend.onrender.com/api/v1', // Backend URL
 });
 
 export const registerUser = (userData) => {
