ac1e108aed8f862386f3f5470537a5e527124991
Sachins0 <sachins.ssingh0@gmail.com>
2024-12-21 09:02:45 +0530

    minor changes

--- server/src/config/server-config.js
+++ server/src/config/server-config.js
@@ -6,4 +6,8 @@ module.exports={
     SALT_ROUNDS: process.env.SALT_ROUNDS,
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_NAME: process.env.DB_NAME,
+    DB_HOST: process.env.DB_HOST
 }
